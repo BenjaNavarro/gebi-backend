59fb53385ce838dda2b9573927506d2d6d71f492
Benjamin Navarro <benjamin.navarro.m@outlook.cl>
2025-12-30 13:41:41 -0300

    Chore: Product Type schema updated

--- src/infrastructure/web/database/models/ProductYpeSchema.ts
+++ src/infrastructure/web/database/models/ProductYpeSchema.ts
@@ -2,7 +2,6 @@ import { Schema } from "mongoose";
 
 export const ProductTypeSchema = new Schema(
     {
-        idTipoProducto: { type: String, required: true, unique: true },
         nombre: { type: String, required: true },
         descripcion: { type: String, required: false },
         estado: { type: Boolean, required: true },
