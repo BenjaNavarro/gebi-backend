5262edfc8879656bc0f9881985e2629824ab8ef8
Benjamin Navarro <benjamin.navarro.m@outlook.cl>
2025-12-30 13:52:23 -0300

    Chore: Client schema updated

--- src/infrastructure/web/database/models/ClientUnitSchema.ts
+++ src/infrastructure/web/database/models/ClientUnitSchema.ts
@@ -2,7 +2,6 @@ import { Schema } from "mongoose";
 
 export const ClientUnitSchema = new Schema(
     {
-        idClienteUnidad: { type: String, required: true, unique: true },
         nombre: { type: String, required: true },
         descripcion: { type: String, required: false },
         estado: { type: Boolean, required: true },
